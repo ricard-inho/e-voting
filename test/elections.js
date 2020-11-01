c31efa484c1df744cd36a0c9dce33704deb33a4b
ricardmc99 <ricardmc99@gmail.com>
2020-11-01 15:25:08 +0100

    web interface

--- test/elections.js
+++ test/elections.js
@@ -1,8 +1,11 @@
 var Elections = artifacts.require("./Elections.sol");
 
 contract("Elections", function(accounts) {
+	var electionInstance;
+
 	it("Initializes with a chairPerson", function() {
 		return Elections.deployed().then(function(instance) {
+			electionInstance = instance;
 			return instance.votersCount();
 		}).then(function(count) {
 			assert.equal(count, 1);
